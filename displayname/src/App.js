e0285b2d35548efad1be8a5774a31a1f1508bb91
sanjay5577 <143835637+sanjay5577@users.noreply.github.com>
2024-01-26 17:35:37 +0000

    display name

---
+++ displayname/src/App.js
@@ -0,0 +1,56 @@
+import "./App.css"
+import { useState } from "react";
+
+const Displayname = ()=>{
+
+  const [issubmit , setIsSubmit]=useState(false);
+  const[inputs , setInputs]=useState({
+    firstname: "",
+    lastname :""
+  });
+
+  const handleChange =(event)=>{
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setInputs( values => ({...values ,[name]:value})); 
+
+  }
+
+  const handleSubmit =(event)=>{
+    event.preventDefault();
+    setIsSubmit(true);
+  }
+
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+      <h1>Full Name Display</h1>
+      <div className="container">
+        <div>
+        <label htmlFor="firstname">First Name:</label>
+        <input 
+        type="text"  
+        required 
+        name="firstname" 
+        value={inputs.firstname} 
+        onChange={handleChange}
+        />
+        </div>
+        <div>
+        <label htmlFor="lastname">Last Name:</label>
+        <input type="text" required name="lastname"value={inputs.lastname} 
+        onChange={handleChange}/>
+        </div>
+      
+      </div>
+      <button >Submit</button>
+      </form>
+
+      {issubmit && <p>Full Name: {inputs.firstname+" "+inputs.lastname}</p>}
+
+    </div>
+  )
+}
+
+export default Displayname;
